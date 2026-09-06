5ba78d7fe7d1748ca6016d7c03fab5e3bf08796f
kodepl <koode9@gmail.com>
2026-09-06 11:08:20 +0200

    chore: rename assets dir (no /_astro footprint)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   site: "https://www.fotograf-przyrody.pl",
   trailingSlash: "always",
   compressHTML: true,
+  build: { assets: "teren" },
   markdown: { rehypePlugins: [rehypeSlug] },
   integrations: [tailwind()],
 });
